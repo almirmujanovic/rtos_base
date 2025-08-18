713df1220fa1e4c025a7ac352fde025f6af2a872
Almir <almir.mujanovic25@gmail.com>
2025-08-18 15:32:45 +0200

    videostream good

--- main/include/mjpeg_stream_task.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "esp_http_server.h"
-static esp_err_t mjpeg_stream_handler(httpd_req_t *req);
-static void start_mjpeg_http_server(void);
-void mjpeg_stream_task(void *pvParameter);
--- main/main.c
+++ main/main.c
@@ -11,22 +11,15 @@
 #include "lwip/ip4_addr.h"
 #include "esp_mac.h"
 #include "esp_system.h"
-//#include "esp_http_server.h"
 #include "mqtt_client.h"
-//#include "websocket_server.h"
+
 
 #include "./include/mqtt_uart_bridge.h"
-
 #include "./include/rtsp_wrapper.h"
 #include "./include/camera_config.h"
 #include "./include/camera_task.h"
-//#include "./include/camera_httpd.h"
 #include "./include/uart_comm.h"
 #include "./include/uart_task.h"
-
-//#include "./include/mjpeg_stream_task.h"
-
-// Define the WebSocket URL used for the server
 
 static const char *TAG = "MAIN";
 
@@ -63,8 +56,8 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
         if (!server_started) {
             server_started = true;
 
-           // uart_init();
-           // xTaskCreatePinnedToCore(uart_task, "UART task", 4096, NULL, 15, NULL, 1);
+            uart_init();
+            xTaskCreatePinnedToCore(uart_task, "UART task", 4096, NULL, 15, NULL, 1);
 
             if (init_camera() != ESP_OK) {
                 ESP_LOGE(TAG, "Camera init failed. Aborting server startup.");
@@ -82,7 +75,7 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
             } else {
                 ESP_LOGE(TAG, "Failed to initialize RTSP server");
             }
-           // mqtt_app_start(); // Start MQTT client
+            mqtt_app_start(); // Start MQTT client
 
         }
     }
@@ -138,48 +131,3 @@ void app_main(void) {
     if ((ret) != 0) { \
         ESP_LOGE(TAG, "Last error %s: 0x%x", message, ret); \
     }
-
-/*
-void app_main(void) {
-
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());   // ✅ Erase NVS partition
-        ESP_ERROR_CHECK(nvs_flash_init());    // ✅ Retry init
-    }
-    wifi_init_sta();
-    uart_init();
-    
-    if (init_camera() != ESP_OK) {
-        ESP_LOGE(TAG, "Camera init failed. Halting app.");
-        return; // Don't start camera task if init fails
-    }
-
-    xTaskCreatePinnedToCore(uart_task, "UART Task", 4096, NULL, 15, NULL, 1);
-   // xTaskCreatePinnedToCore(camera_udp_task, "UDP Camera Task", 8192, NULL, 5, NULL, 1);  
-
-    ESP_LOGI("MAIN: HEAP", "Free heap: %u, internal: %u, PSRAM: %u",
-        (unsigned int)esp_get_free_heap_size(),
-        (unsigned int)heap_caps_get_free_size(MALLOC_CAP_INTERNAL),
-        (unsigned int)heap_caps_get_free_size(MALLOC_CAP_SPIRAM));
-
-    ESP_LOGI(TAG, "[APP] Startup..");
-    ESP_LOGI(TAG, "[APP] Free memory: %" PRId32 " bytes", (int32_t) esp_get_free_heap_size());    
-    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
-
-    esp_log_level_set("*", ESP_LOG_WARN);           // Only warnings and errors
-    esp_log_level_set("MAIN", ESP_LOG_INFO);        // Keep main info
-    esp_log_level_set("MQTT_UART", ESP_LOG_INFO);   // Keep MQTT info
-    esp_log_level_set("UART_TASK", ESP_LOG_WARN);
-
-    esp_log_level_set("MQTT_CLIENT", ESP_LOG_WARN);
-    esp_log_level_set("TRANSPORT_BASE", ESP_LOG_WARN);
-    esp_log_level_set("esp-tls", ESP_LOG_WARN);
-
-    
-   //  This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-   //  * Read "Establishing Wi-Fi or Ethernet Connection" section in
-   //  * examples/protocols/README.md for more information about this function.
-     
-
-}*/
